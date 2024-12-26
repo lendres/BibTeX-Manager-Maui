e85e71b6168ebc8953b67169275ec5965bfbad72
Ikosy <lendres@fifthrace.com>
2024-12-26 10:45:47 -0600

    Create BibEntryViewModel.cs

---
+++ src/User Interface/ViewModels/BibEntryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XSLTProcessorMaui.View_Models
+{
+    class BibEntryViewModel
+    {
+    }
+}
