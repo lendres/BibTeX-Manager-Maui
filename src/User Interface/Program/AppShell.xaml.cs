8906095197f419d2c04f2f4c106a4ba07f49f9b7
Ikosy <lendres@fifthrace.com>
2024-12-27 12:48:57 -0600

    Move menu

--- src/User Interface/Program/AppShell.xaml.cs
+++ src/User Interface/Program/AppShell.xaml.cs
@@ -1,12 +1,104 @@
-﻿using BibTexManager.Views;
+﻿using BibTexManager.ViewModels;
+using BibTexManager.Views;
+using CommunityToolkit.Maui.Views;
+using DigitalProduction.ViewModels;
+using DigitalProduction.Views;
 
 namespace BibTexManager;
 
 public partial class AppShell : Shell
 {
+	#region Fields
+
+	private readonly FilePickerFileType _bibliographyProjectFileType = new(new Dictionary<DevicePlatform, IEnumerable<string>>
+	{
+		{
+			DevicePlatform.WinUI, new[]
+			{
+				".bibproj"
+			}
+		},
+	});
+
+	#endregion
+
+	#region Contruction
+
 	public AppShell()
 	{
 		InitializeComponent();
 		Routing.RegisterRoute(nameof(EditRawBibEntryForm), typeof(EditRawBibEntryForm));
 	}
+
+	#endregion
+
+	#region Menu Events
+
+	async void OnOpen(object sender, EventArgs eventArgs)
+	{
+		PickOptions pickOptions = new()
+		{
+			PickerTitle = "Select an Bibliography Project File",
+			FileTypes   = _bibliographyProjectFileType
+		};
+		FileResult? result = await BrowseForFile(pickOptions);
+		if (result != null)
+		{
+			MainViewModel? viewModel = BindingContext as MainViewModel;
+			viewModel?.OpenProject(result.FullPath);
+		}
+	}
+
+	public static async Task<FileResult?> BrowseForFile(PickOptions options)
+	{
+		try
+		{
+			return await FilePicker.PickAsync(options);
+		}
+		catch
+		{
+			//(Exception exception)
+			//string message = exception.Message;
+			// The user canceled or something went wrong.
+		}
+		return null;
+	}
+
+	async void OnShowProgramOptions(object sender, EventArgs eventArgs)
+	{
+		ProgramOptionsViewModel viewModel = new();
+
+		ProgramOptionsView	view	= new(viewModel);
+		object?				result	= await Shell.Current.ShowPopupAsync(view);
+
+		if (result is bool boolResult && boolResult)
+		{
+		}
+	}
+
+	async void OnHelp(object sender, EventArgs eventArgs)
+	{
+		System.Reflection.Assembly? entryAssembly = System.Reflection.Assembly.GetEntryAssembly();
+		System.Diagnostics.Debug.Assert(entryAssembly != null);
+		string url = DigitalProduction.Reflection.Assembly.DocumentationAddress(entryAssembly);
+		await Launcher.OpenAsync(url);
+	}
+
+	async void OnAbout(object sender, EventArgs eventArgs)
+	{
+		AboutView1 view = new(new AboutViewModel(true));
+		_ = await Shell.Current.ShowPopupAsync(view);
+	}
+
+	/// <summary>
+	/// Exit command.
+	/// </summary>
+	/// <param name="sender">Sender.</param>
+	/// <param name="eventArgs">Event arguments.</param>
+	protected void OnExit(object sender, EventArgs eventArgs)
+	{
+		Application.Current?.Quit();
+	}
+
+	#endregion
 }
--- src/User Interface/Views/MainPage.xaml.cs
+++ src/User Interface/Views/MainPage.xaml.cs
@@ -11,85 +11,13 @@ namespace BibTexManager.Views;
 
 public partial class MainPage : DigitalProductionMainPage
 {
-	#region Fields
 
-	private readonly FilePickerFileType _bibliographyProjectFileType = new(new Dictionary<DevicePlatform, IEnumerable<string>>
-	{
-		{
-			DevicePlatform.WinUI, new[]
-			{
-				".bibproj"
-			}
-		},
-	});
-
-	#endregion
 
 	#region Construction
 
 	public MainPage()
 	{
 		InitializeComponent();
-	}
-
-	#endregion
-
-	#region Menu Events
-
-	async void OnOpen(object sender, EventArgs eventArgs)
-	{
-		PickOptions pickOptions = new()
-		{
-			PickerTitle = "Select an Bibliography Project File",
-			FileTypes   = _bibliographyProjectFileType
-		};
-		FileResult? result = await BrowseForFile(pickOptions);
-		if (result != null)
-		{
-			MainViewModel? viewModel = BindingContext as MainViewModel;
-			viewModel?.OpenProject(result.FullPath);
-		}
-	}
-
-	public static async Task<FileResult?> BrowseForFile(PickOptions options)
-	{
-		try
-		{
-			return await FilePicker.PickAsync(options);
-		}
-		catch
-		{
-			//(Exception exception)
-			//string message = exception.Message;
-			// The user canceled or something went wrong.
-		}
-		return null;
-	}
-
-	async void OnShowProgramOptions(object sender, EventArgs eventArgs)
-	{
-		ProgramOptionsViewModel viewModel = new();
-
-		ProgramOptionsView	view	= new(viewModel);
-		object?				result	= await Shell.Current.ShowPopupAsync(view);
-
-		if (result is bool boolResult && boolResult)
-		{
-		}
-	}
-
-	async void OnHelp(object sender, EventArgs eventArgs)
-	{
-		System.Reflection.Assembly? entryAssembly = System.Reflection.Assembly.GetEntryAssembly();
-		System.Diagnostics.Debug.Assert(entryAssembly != null);
-		string url = DigitalProduction.Reflection.Assembly.DocumentationAddress(entryAssembly);
-		await Launcher.OpenAsync(url);
-	}
-
-	async void OnAbout(object sender, EventArgs eventArgs)
-	{
-		AboutView1 view = new(new AboutViewModel(true));
-		_ = await Shell.Current.ShowPopupAsync(view);
 	}
 
 	#endregion
@@ -104,11 +32,6 @@ public partial class MainPage : DigitalProductionMainPage
 		{
 			{"AddMode",  true},
 		});
-
-
-
-
-
 
 		//ConfigurationsViewModel? configurationsViewModel = BindingContext as ConfigurationsViewModel;
 		//System.Diagnostics.Debug.Assert(configurationsViewModel != null);
