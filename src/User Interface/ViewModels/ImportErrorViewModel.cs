f31250f895fb5fa12644d471b9fb95ddc54571ef
Ikosy <lendres@fifthrace.com>
2025-06-26 21:03:46 -0500

    Start adding import error form

---
+++ src/User Interface/ViewModels/ImportErrorViewModel.cs
@@ -0,0 +1,33 @@
+﻿using BibtexManager;
+using CommunityToolkit.Mvvm.ComponentModel;
+using DigitalProduction.Maui.Controls;
+
+namespace BibTexManager.ViewModels;
+
+public partial class ImportErrorViewModel : ObservableObject
+{
+	#region Construction
+
+	public ImportErrorViewModel()
+	{
+	}
+
+	#endregion
+
+	#region Properties
+
+	[ObservableProperty]
+	public partial string				Message { get; set; }					= "";
+
+	#endregion
+
+	#region Events
+
+
+	#endregion
+
+	#region Methods
+
+
+	#endregion
+}
---
+++ src/User Interface/Views/ImportErrorView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using BibtexManager.Importing;
+using BibTexManager.ViewModels;
+using DigitalProduction.Maui.Controls;
+using DigitalProduction.Maui.Views;
+
+namespace BibTexManager.Views;
+
+public partial class ImportErrorView : PopupView
+{
+	#region Fields
+
+	private readonly ImportErrorViewModel _viewModel;
+
+	#endregion
+
+	#region Construction
+
+	public ImportErrorViewModel(CorrectionViewModel viewModel)
+	{
+		BindingContext	= viewModel;
+		_viewModel		= viewModel;
+		InitializeComponent();
+	}
+
+	#endregion
+
+	#region Events
+
+	async void OnTryAgainButtonClicked(object? sender, EventArgs eventArgs)
+	{
+		await HandleResult(ImportErrorHandlingType.TryAgain);
+	}
+
+	async void OnSkipButtonClicked(object? sender, EventArgs eventArgs)
+	{
+		await HandleResult(ImportErrorHandlingType.Skip);
+	}
+
+	async new void OnCancelButtonClicked(object? sender, EventArgs eventArgs)
+	{
+		await HandleResult(ImportErrorHandlingType.Cancel);
+	}
+
+	private async Task HandleResult(ImportErrorHandlingType result)
+	{
+		//_viewModel.SetResult(result);
+		CancellationTokenSource cancelationTokenSource = new(TimeSpan.FromSeconds(5));
+		await CloseAsync(result, cancelationTokenSource.Token);
+	}
+
+	#endregion
+}
