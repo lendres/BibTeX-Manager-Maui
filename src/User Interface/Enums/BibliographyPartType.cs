416eea668aa67e0835e809dcd8b0f7e661d724f3
Lance A. Endres <lanceendres@gmail.com>
2026-05-14 16:33:39 -0500

    Create BibliographyPartType.cs

---
+++ src/User Interface/Enums/BibliographyPartType.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+/// <summary>
+/// Main parts of the Bibliography.
+/// 
+/// The "Description" attribute can be accessed using Reflection to get a string representing the enumeration type.
+/// </summary>
+namespace BibTeXManager.Enums
+{
+	internal enum BibliographyPartType
+	{
+		/// <summary>Bibliography entries.</summary>
+		[Description("Bibliography Entries")]
+		BibliographyEntries,
+
+		/// <summary>Header of the bibliography.</summary>
+		[Description("Header")]
+		Header,
+
+		/// <summary>String entries.</summary>
+		[Description("String Entries")]
+		StringEntries
+	}
+}
