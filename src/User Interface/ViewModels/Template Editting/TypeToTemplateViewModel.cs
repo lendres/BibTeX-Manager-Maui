e5cab838caa6954c6b994557ea9eacf999d25bd0
Lance A. Endres <lanceendres@gmail.com>
2026-05-30 13:19:07 -0500

    Create TypeToTemplateViewModel.cs

---
+++ src/User Interface/ViewModels/Template Editting/TypeToTemplateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeXManager.ViewModels.Template_Editting
+{
+	internal class TypeToTemplateViewModel
+	{
+	}
+}
