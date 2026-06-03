923ef5d2323b0ca2750febc9fff0307dc043478d
Lance A. Endres <lanceendres@gmail.com>
2026-06-03 12:03:41 -0500

    Add serialization

--- src/BibTex Project/Quality/GroupManager.cs
+++ src/BibTex Project/Quality/GroupManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using DigitalProduction.Xml.Serialization;
+using System.Collections.ObjectModel;
 using System.Xml.Serialization;
 
 namespace BibtexManager;
@@ -24,4 +25,43 @@ public class GroupManager
 
 	#endregion
 
+	#region Methods
+
+	
+
+	#endregion
+
+	#region XML
+
+	/// <summary>
+	/// Write this object to a file to the provided path.
+	/// </summary>
+	/// <param name="path">Path (full path and filename) to write to.</param>
+	/// <exception cref="InvalidOperationException">Thrown when the projects path is not valid.</exception>
+	public void Serialize(string path)
+	{
+		if (!DigitalProduction.IO.Path.PathIsWritable(path))
+		{
+			throw new InvalidOperationException("The file cannot be saved. A valid path must be specified.");
+		}
+		Serialization.SerializeObject(this, path);
+	}
+
+	/// <summary>
+	/// Deserialize an object from a file.
+	/// </summary>
+	/// <typeparam name="T">Type of object to deserialize.</typeparam>
+	/// <param name="file">File to deserialize from.</param>
+	public static T? DeserializeWithoutIncluding<T>(string file)
+	{
+		XmlSerializer serializer = new(typeof(T));
+
+		using (StreamReader streamReader = new(file))
+		{
+			T? deserializedobject = (T?)serializer.Deserialize(streamReader);
+			return deserializedobject;
+		}
+	}
+
+	#endregion
 }
