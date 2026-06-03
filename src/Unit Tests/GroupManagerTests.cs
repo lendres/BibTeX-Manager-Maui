db4f9849686664cbebffcf21d69edbcb1aa0e6f0
Lance A. Endres <lanceendres@gmail.com>
2026-06-03 12:16:26 -0500

    Create GroupManagerTests.cs

---
+++ src/Unit Tests/GroupManagerTests.cs
@@ -0,0 +1,10 @@
+﻿using BibtexManager;
+using BibTeXManager;
+using BibTeXManager.Quality;
+using System.Collections.ObjectModel;
+
+namespace BibTeXManagerUnitTests;
+
+public class GroupManagerTests
+{
+}
