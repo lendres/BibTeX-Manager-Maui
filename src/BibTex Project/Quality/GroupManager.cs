8499be179a50d8207bd18f72a77cc01f008e9930
Lance A. Endres <lanceendres@gmail.com>
2026-06-03 09:26:43 -0500

    Create GroupManager.cs

---
+++ src/BibTex Project/Quality/GroupManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using System.Xml.Serialization;
+
+namespace BibtexManager;
+
+public class XInclude
+{
+	[XmlAttribute("href")]
+	public string Href { get; set; } = string.Empty;
+}
+
+
+public class GroupManager
+{
+
+	#region Fields
+
+	#endregion
+
+	#region Properties
+
+	[XmlArray("fieldprocessorgroups"), XmlArrayItem("include", Namespace = "http://www.w3.org/2001/XInclude")]
+	public ObservableCollection<XInclude> Includes { get; set; } = new();
+
+	#endregion
+
+}
