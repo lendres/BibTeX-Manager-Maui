e8bc9cc17b2924cf76fd526456b7be0a844a158f
Lance A. Endres <lanceendres@gmail.com>
2026-06-03 15:19:23 -0500

    Create GroupManagerIncludeViewModel.cs

---
+++ src/User Interface/ViewModels/Field Quality/GroupManagerIncludeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BibTeXManager.ViewModels;
+
+public partial class GroupManagerIncludeViewModel : ObservableObject
+{
+	[ObservableProperty]
+	public partial bool IsIncluded { get; set; }
+
+	[ObservableProperty]
+	public partial string IncludeName { get; set; } = string.Empty;
+}
